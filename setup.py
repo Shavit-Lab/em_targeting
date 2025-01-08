7d7f6e29d35391a28bbe1d6b48bd59ce99079aa2
Thomas Athey <tom.l.athey@gmail.com>
2025-01-08 17:30:03 -0500

    h5py

--- setup.py
+++ setup.py
@@ -8,12 +8,6 @@ setup(
     packages=["em_targeting"],
     author="Thomas Athey",
     author_email="tom.l.athey@gmail.com",
-    install_requires=[
-        "napari",
-        "numpy",
-        "pandas",
-        "Pillow",
-        "scikit-image",
-    ],
+    install_requires=["napari", "numpy", "pandas", "Pillow", "scikit-image", "h5py"],
     license="MIT",
 )
